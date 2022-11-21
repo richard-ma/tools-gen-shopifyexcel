0e5f844a46d468f5b9c5990cea9d6e110758b049
richard-ma <richard.ma.19850509@gmail.com>
2022-11-21 19:06:02 +0800

    add input file encoding auto detect

--- src/app.py
+++ src/app.py
@@ -3,13 +3,25 @@
 import csv
 import os
 import sys
+from chardet.universaldetector import UniversalDetector
 
 
 class App:
     def __init__(self):
         self.title_line = sys.argv[0]
+        self.encoding = None
         self.counter = 1
         self.workingDir = None
+
+    def _char_detect(self, filename: str):
+        detector = UniversalDetector()
+        detector.reset()
+        for line in open(filename, 'rb'):
+            detector.feed(line)
+            if detector.done:
+                break
+        detector.close()
+        self.encoding = detector.result['encoding']
 
     def printCounter(self, data=None):
         print("[%04d] Porcessing: %s" % (self.counter, str(data)))
@@ -39,23 +51,37 @@ class App:
         else:
             return var
 
-    def readTxtToList(self, filename, encoding="GBK"):
+    def readTxtToList(self, filename, encoding=None):
+        if encoding is None:
+            self.encoding = self._char_detect(filename)
+        else:
+            self.encoding = encoding
+
         data = list()
-        with open(filename, 'r+', encoding=encoding) as f:
+        with open(filename, 'r+', encoding=self.encoding) as f:
             for row in f.readlines():
                 # remove \n and \r
                 data.append(row.replace('\n', '').replace('\r', ''))
         return data
 
-    def readCsvToDict(self, filename, encoding="GBK"):
+    def readCsvToDict(self, filename, encoding=None):
+        if encoding is None:
+            self.encoding = self._char_detect(filename)
+        else:
+            self.encoding = encoding
+
+
         data = list()
-        with open(filename, 'r+', encoding=encoding) as f:
+        with open(filename, 'r+', encoding=self.encoding) as f:
             reader = csv.DictReader(f)
             for row in reader:
                 data.append(row)
         return data
 
-    def writeCsvFromDict(self, filename, data, fieldnames=None, encoding="GBK", newline=''):
+    def writeCsvFromDict(self, filename, data, fieldnames=None, encoding=None, newline=''):
+        if encoding is None and self.encoding is not None:
+            encoding = self.encoding
+
         if fieldnames is None:
             fieldnames = data[0].keys()
 
